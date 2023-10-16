96cfc524634d6d54ba47dc15bdf86c295b0d6df2
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-10-16 15:38:35 +0300

    Add files via upload

---
+++ button.ornek.29/button.ornek.29/button.ornek.29/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button.ornek._29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int aylar = comboBox1.SelectedIndex;
+            aylar++;
+            if (aylar==3 || aylar==4 || aylar==5)
+            {
+                label1.Text = "İlkbahar";
+            }
+            else if (aylar==6 || aylar==7 || aylar==8)
+            {
+                label1.Text = "Yaz";
+            }
+            else if (aylar==9 || aylar==10 || aylar==11)
+            {
+                label1.Text = "Sonbahar";
+            }
+            else
+            {
+                label1.Text = "Kış";
+            }
+
+        }
+    }
+}
